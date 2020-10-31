927a6b72253d800a4387901344e5faae0a0dfadd
f <f>
2020-10-31 16:06:04 +0100

    tsla data ytd

---
+++ 3TERM/ap/getData.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+
+#define the ticker symbol
+tickerSymbol = 'TSLA'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2020-1-1', end='2020-10-30')
+
+#see your data
+
+for col in tickerDf['Close']:
+    print col,
+
