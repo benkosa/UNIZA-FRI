a1092117d520408584f5fe9eb9ce8e6ba32e349c
f <f>
2020-11-16 15:35:39 +0100

    tdoc data

--- 3TERM/ap/getData.py
+++ 3TERM/ap/getData.py
@@ -1,14 +1,15 @@
 import yfinance as yf
 
 #define the ticker symbol
-tickerSymbol = 'TSLA'
+tickerSymbol = 'TDOC'
 
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2020-1-1', end='2020-10-30')
+#tickerDf = tickerData.history(period='1d', start='2020-1-1', end='2020-10-30')
 
+tickerDf = tickerData.history(period='ytd')
 #see your data
 for col in tickerDf['Close']:
-    print(col)
+    print(col),
